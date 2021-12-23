44dbfd63fcd83e6183a0bb1ccd3168551bdcd114
Gryzmol88 <mnadrajkowski@wp.pl>
2021-12-23 10:33:32 +0100

    add current choices

--- order/models.py
+++ order/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 
 class Order(models.Model):
-    STATUS_CHOICES = (
+    STATUS_CHOICES_ORDER = (
         ('W przygotowaniu', 'W przygotowaniu'),
         ('Zamówiono', 'Zamówiono'),
         ('Wysłano', 'Wysłano'),
         ('Dostarczono', 'Dostarczono'),
          )
+
+    CURRENT_CHOICES = (
+        ('PLN', 'PLN'),
+        ('USD', 'USD'),
+    )
+
     name = models.CharField(max_length=200)
     date_of_order = models.DateField()
-    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='W przygotowaniu')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES_ORDER, default='W przygotowaniu')
     product_price = models.IntegerField()
     transport_price = models.IntegerField()
+    current_transport_price = models.CharField(max_length=3, choices=CURRENT_CHOICES, default='USD')
     customs_price = models.IntegerField()
     total_weight = models.IntegerField()
 
