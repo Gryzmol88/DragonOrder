d7bd811a51d6689cd45dad943a07bf29521974a6
Michu <mnadrajkowski@wp.pl>
2021-12-26 20:14:00 +0100

    add order list and templates html

--- order/views.py
+++ order/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Order
 
 def index(request):
     return render(request, 'order/index.html')
 
 def order_list(request):
-    return render(request, 'order/order_list.html')
+    orders = Order.objects.all
+    context = {'orders': orders}
+    return render(request, 'order/order_list.html', context)
