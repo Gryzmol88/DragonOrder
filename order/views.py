92425a55bc6ffe337c64abc8cf1a0c2f15fad41b
Michu <mnadrajkowski@wp.pl>
2021-12-27 21:18:00 +0100

    add order list and templates html

--- order/views.py
+++ order/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import Order
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     return render(request, 'order/index.html')
 
 def order_list(request):
-    orders = Order.objects.all
-    context = {'orders': orders}
+    object_list = Order.objects.all()
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get('page')
+    try:
+        orders = paginator.page(page)
+    except PageNotAnInteger:
+        orders = paginator.page(1)
+    except EmptyPage:
+        orders = paginator.page(paginator.num_pages)
+    context = {'orders': orders, 'page': page}
     return render(request, 'order/order_list.html', context)
